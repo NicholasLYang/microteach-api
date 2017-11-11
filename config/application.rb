661f50f3ad9f580bf0477dac0e32b9a07505f5db
Nicholas Yang <ny585@nyu.edu>
2017-11-11 15:55:24 -0500

    Revert "seed if production"
    This reverts commit fdee1d18c4739e895f2c3a28d60d00c6d899d252.

--- config/application.rb
+++ config/application.rb
@@ -24,9 +24,7 @@ module Api
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.after_initialize do
-      Rails.application.load_seed if Rails.env.production?
-    end
+
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
